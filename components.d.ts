c6e289ebad3e1c4c94ee8f1eff48ba4067801f7f
BraumAce <1693717911@qq.com>
2024-04-02 22:35:03 +0800

    添加通用布局

--- components.d.ts
+++ components.d.ts
@@ -7,9 +7,17 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
+    BlogCardList: typeof import('./src/components/BlogCardList.vue')['default']
+    CommentList: typeof import('./src/components/CommentList.vue')['default']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
+    MessageDefalutGrid: typeof import('./src/components/MessageDefalutGrid.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
+    TeamCardList: typeof import('./src/components/TeamCardList.vue')['default']
+    TeamMemberCardList: typeof import('./src/components/TeamMemberCardList.vue')['default']
+    UserCardList: typeof import('./src/components/UserCardList.vue')['default']
+    UserCommentList: typeof import('./src/components/UserCommentList.vue')['default']
+    UserLikeList: typeof import('./src/components/UserLikeList.vue')['default']
     VanButton: typeof import('vant/es')['Button']
   }
 }
